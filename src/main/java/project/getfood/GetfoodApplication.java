e0be821f1fdce821545dca821853beb732f8f690
Pavan Potnuru <potnurupavankumar843@gmail.com>
2025-07-31 15:18:42 +0530

    Initial commit

---
+++ src/main/java/project/getfood/GetfoodApplication.java
@@ -0,0 +1,13 @@
+package project.getfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetfoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetfoodApplication.class, args);
+	}
+
+}
